f1dca6d55e8fed51161a54fcaae3f0f823cbdcea
ShengHua Wu <fantasy0404@gmail.com>
2017-05-17 18:07:21 +0800

    First commit.

---
+++ TwitterImage/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  TwitterImage
+//
+//  Created by ShengHua Wu on 17/05/2017.
+//  Copyright © 2017 ShengHua Wu. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+        window = UIWindow(frame: UIScreen.main.bounds)
+        window?.backgroundColor = .white
+        
+        let navigationController = UINavigationController(rootViewController: ViewController())
+        window?.rootViewController = navigationController
+        window?.makeKeyAndVisible()
+        
+        return true
+    }
+}
+
---
+++ TwitterImage/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TwitterImage
+//
+//  Created by ShengHua Wu on 17/05/2017.
+//  Copyright © 2017 ShengHua Wu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    // MARK: View Life Cycle
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .blue
+    }
+}
+
